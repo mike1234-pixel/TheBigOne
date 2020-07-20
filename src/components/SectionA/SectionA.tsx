5a4ae2999070a884ec1a186e892ff6030930690a
Michael Tandy <mike.tandy.1@gmail.com>
2020-07-20 20:12:13 +0100

    added some grid squares in section A using css grids, in SectionA.tsx and SectionA.scss.

--- src/components/SectionA/SectionA.tsx
+++ src/components/SectionA/SectionA.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./SectionA.scss";
 
 const SectionA = () => {
-  return <div className="section-a"></div>;
+  return (
+    <div className="section-a grid-container">
+      <div className="grid-tem grid-item-one">item</div>
+      <div className="grid-tem grid-item-two">item</div>
+      <div className="grid-tem grid-item-three">item</div>
+    </div>
+  );
 };
 
 export default SectionA;
+
+// hover over text to reveal a picture
