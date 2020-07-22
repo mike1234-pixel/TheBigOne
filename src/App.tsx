a3a8f464aa0aa7b168385eec887fb9bb9b1f097f
Michael Tandy <mike.tandy.1@gmail.com>
2020-07-22 13:47:37 +0100

    using useStore and store.getState() in SectionA.tsx to access redux state.

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,7 @@ import SectionC from "./components/SectionC/SectionC";
 import SectionD from "./components/SectionD/SectionD";
 import { connect } from "react-redux";
 
-// press button on nav bar, the button would have to be a component in app and passed down as a prop
-// change state of entire app to darkMode: true
-// pass state down as props and various components change color
-
 function App(props) {
-  // const [darkMode, setDarkMode] = useState(true);
-
   const loremText =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
--- src/components/SectionA/SectionA.tsx
+++ src/components/SectionA/SectionA.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import "./SectionA.scss";
+import { useStore } from "react-redux";
 
 const SectionA = () => {
   const loremText =
     "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.";
+
+  const store = useStore();
+
+  const state = store.getState();
+  // access darkMode with state.darkMode
 
   return (
     <div className="section-a grid-container">
       <div className="grid-item grid-item-one">
         <h2>item</h2>
         <p>{loremText}</p>
+        <p>{state.darkMode ? `DARK MODE ON` : `DARK MODE OFF`}</p>
       </div>
 
       <div className="grid-item grid-item-two">
