654c783732f7d5012e4fe0d93cf9325524ef549d
Michael Tandy <mike.tandy.1@gmail.com>
2020-07-22 13:34:08 +0100

    moved darkMode state management from react hooks to redux store in App.tsx and index.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.scss";
 import Nav from "./components/Nav/Nav";
 import Carousel from "./components/Carousel/Carousel";
@@ -7,25 +7,29 @@ import SectionA from "./components/SectionA/SectionA";
 import SectionB from "./components/SectionB/SectionB";
 import SectionC from "./components/SectionC/SectionC";
 import SectionD from "./components/SectionD/SectionD";
+import { connect } from "react-redux";
 
 // press button on nav bar, the button would have to be a component in app and passed down as a prop
 // change state of entire app to darkMode: true
 // pass state down as props and various components change color
 
-function App() {
-  const [darkMode, setDarkMode] = useState(true);
+function App(props) {
+  // const [darkMode, setDarkMode] = useState(true);
 
   const loremText =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
   const darkModeButton = (
-    <button className="dark-mode-btn" onClick={() => setDarkMode(!darkMode)}>
-      {darkMode ? `Light Mode` : `Dark Mode`}
+    <button
+      className="dark-mode-btn"
+      onClick={() => props.dispatch({ type: "TOGGLE_DARKMODE" })}
+    >
+      {props.darkMode ? `Light Mode` : `Dark Mode`}
     </button>
   );
   return (
     <div className="App">
-      <Nav darkModeButton={darkModeButton} darkMode={darkMode} />
+      <Nav darkModeButton={darkModeButton} darkMode={props.darkMode} />
       <Carousel />
       <CarouselTint />
       <SectionA />
@@ -36,4 +40,8 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  darkMode: state.darkMode,
+});
+
+export default connect(mapStateToProps)(App);
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,32 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+// set initialState of redux store here
+const initialState = {
+  darkMode: true,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "TOGGLE_DARKMODE":
+      return {
+        darkMode: !state.darkMode,
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
