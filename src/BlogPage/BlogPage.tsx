307968646a059877203a2e5e0ec3e3a9fec483c4
Michael Tandy <mike.tandy.1@gmail.com>
2020-07-29 18:23:48 +0100

    connected React frontend to node backend in BlogPage.tsx.

--- src/BlogPage/BlogPage.tsx
+++ src/BlogPage/BlogPage.tsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./BlogPage.scss";
 
 const BlogPage = () => {
-  return (
-    <div>
-      <h1>Blog Page</h1>
-    </div>
-  );
-};
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState<any[]>([]);
 
+  // useEffect similar to componentDidMount
+  useEffect(() => {
+    // window.fetch = built-in ajax api (does the same as axios)
+    fetch("http://localhost:4000/blogEntries")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.data.blogEntries);
+          console.log(items);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error:</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        {items.map((item) => (
+          <div className="blog-post">
+            <p className="blog-title">{item.title}</p>
+            <p className="blog-content">{item.content}</p>
+            <p className="blog-date">{item.date}</p>
+          </div>
+        ))}
+        <h1>success</h1>
+      </div>
+    );
+  }
+};
 export default BlogPage;
